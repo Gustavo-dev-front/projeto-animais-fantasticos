6c3d160b2804ca2bccd501fbc5a196afa1e20ac9
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-06-28 21:23:19 -0300

    AnimaNumeros transformado em classe.

--- js/modules/animaNumeros.js
+++ js/modules/animaNumeros.js
@@ -1,30 +1,36 @@
-export default function animaNumeros() {
-    const numbersToAnimate = Array.from(document.querySelectorAll('.container-numeros span'));
-    const observer = new MutationObserver(handleObserver);
-    const elementToObserve = document.querySelector('#numeros');
-    observer.observe(elementToObserve, { attributes: true });
+export default class AnimaNumeros {
+  constructor(elementsToAnimate, containerToObserve) {
+    this.elementsToAnimate = document.querySelectorAll(elementsToAnimate);
+    this.containerToObserve = document.querySelector(containerToObserve);
+  }
 
-    function animar() {
-        numbersToAnimate.forEach((n) => {
-            const total = +n.innerText;
-            let acumulator = 0;
+  animar() {
+    this.elementsToAnimate.forEach((n) => {
+      const total = +n.innerText;
+      let accumulator = 0;
 
-            const timer = setInterval(() => {
-                n.innerText = Math.floor(acumulator += (total * 0.01));
-                if (acumulator >= total) {
-                    n.innerText = total;
-                    clearInterval(timer);
-                }
-            }, 20 * Math.random());
-        });
-    }
+      const timer = setInterval(() => {
+        n.innerText = Math.floor((accumulator += total * 0.01));
+        if (accumulator >= total) {
+          n.innerText = total;
+          clearInterval(timer);
+        }
+      }, 20 * Math.random());
+    });
+  }
 
-    function handleObserver() {
-        observer.disconnect();
-        animar();
-    }
+  handleObserver() {
+    this.observer.disconnect();
+    this.animar();
+  }
 
+  addObserver() {
+    this.handleObserver = this.handleObserver.bind(this);
+    this.observer = new MutationObserver(this.handleObserver);
+    this.observer.observe(this.containerToObserve, { attributes: true });
+  }
+
+  init() {
+    this.addObserver();
+  }
 }
-
-
-
