5fd9e23416a328ea2b57c0bb3961f035b61a98d2
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-06-29 21:20:54 -0300

    Classe aprimorada

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -1,19 +1,27 @@
 export default class ScrollAnimation {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.animateSections = this.animateSections.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
+    this.halfHeightWindow = window.innerHeight / 2;
+    this.distances;
   }
 
-  animateSections() {
-    this.sections.forEach((section) => {
-      const sectionDistanceToTop = section.getBoundingClientRect().top;
-      const halfHeightWindow = window.innerHeight / 2;
-      if (sectionDistanceToTop - halfHeightWindow <= 0) section.classList.add("ativo");
+  getDistance() {
+    this.distances = [...this.sections].map((section) => {
+      return { element: section, offset: section.offsetTop - this.halfHeightWindow };
+    });
+  }
+
+  checkDistance() {
+    this.distances.forEach((item) => {
+      if (item.offset <= window.scrollY) item.element.classList.add("ativo");
     });
   }
 
   init() {
-    window.addEventListener("scroll", this.animateSections);
+    this.getDistance();
+    console.log(window);
+    window.addEventListener("scroll", this.checkDistance);
     return this;
   }
 }
