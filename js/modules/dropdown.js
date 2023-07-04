0e2160ac0bd9742c47755799abc07aaf52f77ba3
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-07-04 20:41:57 -0300

    Dropdown refatorado

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,19 +1,23 @@
-import outsideClick from './outsideClick.js';
+import outsideClick from "./outsideClick.js";
 
-export default function initDropdown() {
-    const triggerDropdown = document.querySelector('[data-dropdown-menu]');
-    const events = ['click', 'touchstart'];
-    events.forEach((event) => {
-        triggerDropdown.addEventListener(event, handleClick);
+export default class Dropdown {
+  constructor(triggerDropdown, events) {
+    this.triggerDropdown = document.querySelector(triggerDropdown);
+    this.events = events;
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const display = this.triggerDropdown.classList.contains("ativo");
+    if (!display) this.triggerDropdown.classList.add("ativo");
+
+    outsideClick(this.triggerDropdown, this.events);
+  }
+
+  init() {
+    this.events.forEach((event) => {
+      this.triggerDropdown.addEventListener(event, this.handleClick);
     });
-
-    function handleClick(event) {
-        event.preventDefault();
-        const display = this.classList.contains('ativo');
-        if (!display)
-            this.classList.add('ativo');
-
-        outsideClick(this, events);
-    }
+  }
 }
-
